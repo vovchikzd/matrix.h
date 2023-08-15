5b9e517c4b9d1bed22fcd4ec7ff796c64340a549
Vladimir Zdorenko <vovchikzd@gmail.com>
2023-08-15 21:19:14 +0300

    .

--- matrix.h
+++ matrix.h
@@ -4,10 +4,13 @@
 #include <memory>
 #include <array>
 
-template <typename T, size_t M, size_t N>
+template <typename T, size_t Rows, size_t Columns,
+         template <typename> class Allocator = std::allocator>
 class Matrix {
-    const size_t capacity = M + N;
+    public:
+    const size_t capacity = Rows + Columns;
     size_t size = 0;
+    //Allocator = 
 
 public:
     Matrix() = default;
@@ -17,8 +20,10 @@ public:
     void push_back(const T*, const T*);
     Matrix(const T*, const T*);
 
-    void push_back(const std::array<T, N>&);
+    /* What size? Random? So, another template
+    void push_back(const std::array<T, Columns>&);
     Matrix(const std::array<T, N>&);
+    */
 
     template <template <typename> class Container>
     void push_back(const Container<T>&);
@@ -26,44 +31,57 @@ public:
     template <template <typename> class Container>
     Matrix(const Container<T>&);
 
+
     ~Matrix() = default; // don't forget
+
+    T determinant();
 
 };
 
-template <typename T, size_t M, size_t N>
-void Matrix<T, M, N>::push_back(const T& value) {
+template <typename T, size_t Rows, size_t Columns,
+         template <typename> class Allocator>
+void Matrix<T, Rows, Columns, Allocator>::push_back(const T& value) {
 
 }
 
-template <typename T, size_t M, size_t N>
-void Matrix<T, M, N>::push_back(const T* begin, const T* end) {
+template <typename T, size_t Rows, size_t Columns,
+         template <typename> class Allocator>
+void Matrix<T, Rows, Columns, Allocator>::push_back(const T* begin, const T* end) {
     for(; begin < end; ++begin) {
         this->push_back(*begin);
     }
 }
 
-template <typename T, size_t M, size_t N>
-void Matrix<T, M, N>::push_back(const std::array<T, N>& array) {
+/*
+template <typename T, size_t Rows, size_t N>
+void Matrix<T, Rows, N>::push_back(const std::array<T, N>& array) {
     for(auto it = array.begin(), end = array.end(); it != end; ++it) {
         this->push_bak(*it);
     }
 }
+*/
 
-template <typename T, size_t M, size_t N>
+template <typename T, size_t Rows, size_t Columns,
+         template <typename> class Allocator>
 template <template <typename> class Container>
-void Matrix<T, M, N>::push_back(const Container<T>& container) {
+void Matrix<T, Rows, Columns, Allocator>::push_back(const Container<T>& container) {
     for(auto it = container.begin(), end = container.end(); it != end; ++it) {
         this->push_back(*it);
     }
 }
 
-template <typename T, size_t M, size_t N, size_t K>
-Matrix<T, M, K> operator*(const Matrix<T, M, N>&, const Matrix<T, N, K>&) {
+template <typename T, size_t Rows, size_t K, size_t Columns>
+Matrix<T, Rows, Columns> operator*(const Matrix<T, Rows, K>&, const Matrix<T, K, Columns>&) {
     
 }
 
-template <typename T, size_t M, size_t N>
-Matrix<T, M, N> operator+(const Matrix<T, M, N>&, const Matrix<T, M, N>&) {
+template <typename T, size_t Rows, size_t Columns>
+Matrix<T, Rows, Columns> operator+(const Matrix<T, Rows, Columns>&, const Matrix<T, Rows, Columns>&) {
+
+}
+
+template <typename T, size_t Rows>
+T determinant(Matrix<T, Rows, Rows>& matrix) {
 
 }
 
