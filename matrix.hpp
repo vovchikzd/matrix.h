f47c1132bb0e75e5833b8da39f75587d8e7a3a76
Vladimir Zdorenko <vovchikzd@gmail.com>
2024-01-29 06:52:59 +0300

    Rewrite all, delete "Container" parameter, write determinant(not determinant_impl)

--- matrix.hpp
+++ matrix.hpp
@@ -1,50 +1,52 @@
-#ifndef MATRIX_H_
-#define MATRIX_H_
+#ifndef WRAPPER_MATRIX_H_
+#define WRAPPER_MATRIX_H_
 
 #include <initializer_list>
 #include <vector>
-#include <iterator>
+// #include <iterator>
 #include <stdexcept>
 // #include <utility>
 
-// TODO(#3): Determinant function
 // TODO(#5): Transpose method
 // TODO(#6): Identity matrix method
 // TODO(#7): Invertible matrix function
 
-template <typename T, size_t Rows, size_t Columns,
-          typename Container = std::vector<T>>
+template <size_t Rows, size_t Columns, typename T>
 class Matrix final {
  private:
   const size_t cap_ = Rows * Columns;
+  std::vector<T> container_;
   size_t sz_ = 0;
-  Container container_;
+
+  template <bool>
+  T determinant_impl() = delete;
+
+  template <>
+  T determinant_impl<true>() {
+    // TODO(#3): Determinant function
+  }
 
  public:
   Matrix() = default;
 
-  Matrix(const T& value): sz_(cap_), container_(cap_, value) {}
+  explicit Matrix(const T &value) : container_(cap_, value), sz_(cap_) {}
 
-  Matrix(std::initializer_list<T> ilist): container_(ilist) {
-    auto size = container_.size();
-    sz_ = size >= cap_ ? cap_ : size;
-  }
+  Matrix(std::initializer_list<T> ilist)
+      : container_(ilist),
+        sz_(container_.size() >= cap_ ? cap_ : container_.size()) {}
 
-  Matrix(T *begin, T *end): container_(begin, end) {
-    auto size = container_.size();
-    sz_ = size >= cap_ ? cap_ : size;
-  }
+  Matrix(T *begin, T *end)
+      : container_(begin, end),
+        sz_(container_.size() >= cap_ ? cap_ : container_.size()) {}
 
   template <typename InputIt>
-  Matrix(InputIt begin, InputIt end): container_(begin, end) {
-    auto size = container_.size();
-    sz_ = size >= cap_ ? cap_ : size;
-  }
+  Matrix(InputIt begin, InputIt end)
+      : container_(begin, end),
+        sz_(container_.size() >= cap_ ? cap_ : container_.size()) {}
 
-  Matrix(const Container &other_container): container_(other_container) {
-    auto size = container_.size();
-    sz_ = size >= cap_ ? cap_ : size;
-  }
+  Matrix(const std::vector<T> &other_container)
+      : container_(other_container),
+        sz_(container_.size() >= cap_ ? cap_ : container_.size()) {}
 
   size_t size() const { return sz_; }
 
@@ -54,28 +56,7 @@ class Matrix final {
 
   size_t columns() const { return Columns; }
 
-  Matrix<T, Rows, Columns, Container> &operator+=(
-      const Matrix<T, Rows, Columns, Container> &matrix) {
-    for (size_t ind = 0; ind < sz_; ++ind) {
-      container_[ind] += matrix.container_[ind];
-    }
-    return *this;
-  }
-
-  Matrix<T, Rows, Columns, Container> &operator-=(
-      const Matrix<T, Rows, Columns, Container> &matrix) {
-    for (size_t ind = 0; ind < sz_; ++ind) {
-      container_[ind] -= matrix.container_[ind];
-    }
-    return *this;
-  }
-
-  Matrix<T, Rows, Columns, Container> &operator*=(const T &value) {
-    for (auto& val: container_) {
-      val *= value;
-    }
-    return *this;
-  }
+  T determinant() const { return determinant_imp<Rows == Columns>(); }
 
   T &operator()(size_t row, size_t column) {
     return container_[column + (row * Columns)];
@@ -89,107 +70,117 @@ class Matrix final {
     if (row >= Rows || column >= Columns) {
       throw std::out_of_range("Invalid index");
     }
-    return values[column + (row * Columns)];
+    return container_[column + (row * Columns)];
   }
 
   const T &at(size_t row, size_t column) const {
     if (row >= Rows || column >= Columns) {
       throw std::out_of_range("Invalid index");
     }
-    return values[column + (row * Columns)];
+    return container_[column + (row * Columns)];
   }
 
+  Matrix<Rows, Columns, T> &operator+=(const Matrix<Rows, Columns, T> &matrix) {
+    for (size_t ind = 0; ind < sz_; ++ind) {
+      container_[ind] += matrix.container_[ind];
+    }
+    return *this;
+  }
+
+  Matrix<Rows, Columns, T> &operator-=(const Matrix<Rows, Columns, T> &matrix) {
+    for (size_t ind = 0; ind < sz_; ++ind) {
+      container_[ind] -= matrix.container_[ind];
+    }
+    return *this;
+  }
+
+  template <typename U>
+  Matrix<Rows, Columns, T> &operator*=(const U &value) {
+    for (auto &val : container_) {
+      val *= value;
+    }
+    return *this;
+  }
 };
 
-template <typename T, size_t Rows, size_t Columns,
-          template <typename> class Alloc>
-bool operator==(Matrix<T, Rows, Columns, Alloc> &left,
-                Matrix<T, Rows, Columns, Alloc> &right) {
-  for (size_t i = 0; i < left.size(); ++i) {
-    if (left[i] != right[i]) return false;
+template <size_t Rows, size_t Columns, typename T>
+bool operator==(Matrix<Rows, Columns, T> &left,
+                Matrix<Rows, Columns, T> &right) {
+  if (left.size() != right.size()) {
+    return false;
   }
+
+  for (size_t row = 0; row < Rows; ++row) {
+    for (size_t column = 0; column < Columns; ++column) {
+      if (left(row, column) != right(row, column)) {
+        return false;
+      }
+    }
+  }
+
   return true;
 }
 
-template <typename T, size_t Rows, size_t Columns,
-          template <typename> class Allocator>
-Matrix<T, Rows, Columns, Allocator> operator*(
-    const Matrix<T, Rows, Columns, Allocator> &matrix, const T &value) {
-  auto copy = matrix;
+template <size_t Rows, size_t Columns, typename T, typename U>
+Matrix<Rows, Columns, T> operator*(const Matrix<Rows, Columns, T> &matrix,
+                                   const U &value) {
+  Matrix<Rows, Columns, T> copy = matrix;
   copy *= value;
   return copy;
 }
 
-template <typename T, size_t Rows, size_t Columns,
-          template <typename> class Allocator>
-Matrix<T, Rows, Columns, Allocator> operator*(
-    const T &value, const Matrix<T, Rows, Columns, Allocator> &matrix) {
-  auto copy = matrix;
+template <size_t Rows, size_t Columns, typename T, typename U>
+Matrix<Rows, Columns, T> operator*(const U &value,
+                                   const Matrix<Rows, Columns, T> &matrix) {
+  Matrix<Rows, Columns, T> copy = matrix;
   copy *= value;
   return copy;
 }
 
-template <typename T, size_t Rows, size_t K, size_t Columns,
-          template <typename> class Allocator>
-Matrix<T, Rows, Columns, Allocator> operator*(
-    const Matrix<T, Rows, K, Allocator> &left,
-    const Matrix<T, K, Columns, Allocator> &right) {
-  Matrix<T, Rows, Columns, Allocator> result;
+template <size_t Rows, size_t K, size_t Columns, typename T>
+Matrix<Rows, Columns, T> operator*(const Matrix<Rows, K, T> &left,
+                                   const Matrix<K, Columns, T> &right) {
+  std::vector<T> pre_result;
+  pre_result.reserve(Rows * Columns);
   for (size_t row = 0; row < Rows; ++row) {
     for (size_t column = 0; column < Columns; ++column) {
       T sum = 0;
       for (size_t k = 0; k < K; ++k) {
         sum += left(row, k) * right(k, column);
       }
-      result.push_back(sum);
+      pre_result.push_back(sum);
     }
   }
+  Matrix<Rows, Columns, T> result(pre_result);
   return result;
 }
 
-template <typename T, size_t Rows, size_t Columns,
-          template <typename> class Allocator>
-Matrix<T, Rows, Columns, Allocator> operator+(
-    const Matrix<T, Rows, Columns, Allocator> &left,
-    const Matrix<T, Rows, Columns, Allocator> &right) {
-  auto copy = left;
+template <size_t Rows, size_t Columns, typename T>
+Matrix<Rows, Columns, T> operator+(const Matrix<Rows, Columns, T> &left,
+                                   const Matrix<Rows, Columns, T> &right) {
+  Matrix<Rows, Columns, T> copy = left;
   copy += right;
   return copy;
 }
 
-template <typename T, size_t Rows, size_t Columns,
-          template <typename> class Allocator>
-Matrix<T, Rows, Columns, Allocator> operator-(
-    const Matrix<T, Rows, Columns, Allocator> &left,
-    const Matrix<T, Rows, Columns, Allocator> &right) {
-  auto copy = left;
+template <size_t Rows, size_t Columns, typename T>
+Matrix<Rows, Columns, T> operator-(const Matrix<Rows, Columns, T> &left,
+                                   const Matrix<Rows, Columns, T> &right) {
+  Matrix<Rows, Columns, T> copy = left;
   copy -= right;
   return copy;
 }
 
-template <typename T, size_t Rows, size_t Columns,
-          template <typename> class Allocator>
-Matrix<T, Rows, Columns, Allocator> operator+(
-    Matrix<T, Rows, Columns, Allocator> &matrix) {
+template <size_t Rows, size_t Columns, typename T>
+Matrix<Rows, Columns, T> operator+(Matrix<Rows, Columns, T> matrix) {
   return matrix;
 }
 
-template <typename T, size_t Rows, size_t Columns,
-          template <typename> class Allocator>
-Matrix<T, Rows, Columns, Allocator> operator-(
-    Matrix<T, Rows, Columns, Allocator> &matrix) {
+template <size_t Rows, size_t Columns, typename T>
+Matrix<Rows, Columns, T> operator-(Matrix<Rows, Columns, T> &matrix) {
   auto copy = matrix;
   copy *= -1;
   return copy;
 }
-
-/*
-template <typename T, size_t Size, template <typename> class Allocator>
-T determinant(Matrix<T, Size, Size, Allocator>& matrix) {
-    // implement
-}
-*/
-
-emun class MatrixType {};
 
 #endif
